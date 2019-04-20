eff0f793dab681cdf9e30e451aeffd983341ab21
evgeny.pekhterev <evgeny.pekhterev@rtlabs.ru>
2019-04-20 20:15:08 +0300

    Feign clients setup

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloController {
 
-  @RequestMapping("/hello")
-  public String index() {
-    return "hello";
-  }
+    TempClient client;
 
+    @RequestMapping("/hello")
+    public String index() {
+        //return "hello";
+        return client.get(2).toString();
+    }
 }
--- demo_spring_data/src/main/java/com/example/demo/service/impl/TempServiceImpl.java
+++ demo_spring_data/src/main/java/com/example/demo/service/impl/TempServiceImpl.java
@@ -1,7 +1,8 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.model.Temp;
 import com.example.demo.repository.CrudTempRepository;
+import com.example.demo.service.TempService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
