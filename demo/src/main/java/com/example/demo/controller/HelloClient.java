6eaf0bccd1e34dfa94cbbd094110717c70afd3a2
evgeny.pekhterev <evgeny.pekhterev@rtlabs.ru>
2019-04-20 21:31:16 +0300

    bugfix hello

--- demo/src/main/java/com/example/demo/controller/HelloClient.java
+++ demo/src/main/java/com/example/demo/controller/HelloClient.java
@@ -2,10 +2,15 @@ package com.example.demo.controller;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("hello")
 public interface HelloClient {
 
     @GetMapping("/hello")
-    String getHello();
+    String greetings();
+
+    @RequestMapping("/hello/{id}")
+    String greetingsFor(@PathVariable("id") int id);
 }
--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,17 @@ public class HelloController {
     TempClient client;
 
     @RequestMapping("/hello")
-    public String index() {
-        //return "hello";
-        return client.get(2).toString();
+    public String greetings() {
+        return "Hello!";
+    }
+
+    @RequestMapping("/hello/{id}")
+    public String greetingsFor(@PathVariable("id") int id) {
+        String greetings = "Hello, dear %s, welcome to the jungle!";
+        try {
+            return String.format(greetings, client.get(id).getName());
+        } catch (Exception e) {
+            return String.format(greetings, "NA");
+        }
     }
 }
