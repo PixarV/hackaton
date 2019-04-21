dc183076cf3dd5126af4b2fb9555272e10eb9aa4
ritt <margarita.fedotova@rtlabs.ru>
2019-04-21 15:31:06 +0000

    strange version

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,8 @@ public class HelloController {
     @Autowired
     TempClient client;
 
-    @RequestMapping("/hello")
-    public String index() {
+    @GetMapping(value = "/{id}")
+    public String hello(@PathVariable("id") int id) {
 //        return "hello";
         return client.get(2);
     }
